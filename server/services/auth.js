2808b3257b9f9728301787521d2cd17b238f6833
edensmt <epochws@gmail.com>
2019-09-05 13:54:43 -0600

    fix env variable NAMESPACE

--- server/services/auth.js
+++ server/services/auth.js
@@ -1,5 +1,8 @@
 const jwt = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
+const config = require("../config");
+
+const NAMESPACE = config.NAMESPACE;
 
 //MIDDLEWARE
 exports.checkJWT = jwt({
@@ -21,7 +24,7 @@ exports.checkJWT = jwt({
 exports.checkRole = role => (req, res, next) => {
   const user = req.user;
 
-  if (user && user[process.env.NAMESPACE + "/role"] === role) {
+  if (user && user[NAMESPACE + "/role"] && user[NAMESPACE + "/role"] === role) {
     next();
   } else {
     return res.status(401).send({
